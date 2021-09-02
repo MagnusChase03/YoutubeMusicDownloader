65ba0707e646dfa930b2f0ca918ee1646857e505
Zachary Ness <magnuschase03@pop-os.localdomain>
2021-09-02 16:14:03 -0500

    Added option to download mp4/mp3

--- Download.cpp
+++ Download.cpp
@@ -1,8 +1,8 @@
 /*
 
 Author: Zachary Ness
-Desc: A script that downloads music from youtube
-Inputs: Youtube URL, format number, and name of mp3 file
+Desc: A script that downloads mp3 or mp4 from youtube
+Inputs: Youtube URL, output file, and what you want to download (mp4/mp3)
 Outputs: None
 
 */
@@ -14,20 +14,32 @@ Outputs: None
 
 using namespace std;
 
+string displayVideoFormats(string videoURL);
 void downloadVideo(string videoURL, string videoFormat);
+void downloadVideo(string videoURL, string videoFormat, string mp4Name);
 void convertVideo(string mp3Name);
 
 int main(int argc, char** argv) {
 
     if (argc < 4) {
 
-        cout << "Usage: ./Download [URL] [Format] [MP3 Name]" << endl;
+        cout << "Usage: ./Download [V(ideo)/A(udio)] [URL] [Output File]" << endl;
         return 1;
 
     }
 
-    downloadVideo(argv[1], argv[2]);
-    convertVideo(argv[3]);
+    string videoFormat = displayVideoFormats(argv[2]);
+    string audio = "A";
+    if (argv[1] == audio.c_str()) {
+
+        downloadVideo(argv[2], videoFormat);
+        convertVideo(argv[3]);
+
+    } else {
+
+        downloadVideo(argv[2], videoFormat, argv[3]);
+
+    }
 
     return 0;
 
@@ -35,7 +47,29 @@ int main(int argc, char** argv) {
 
 /*
 
-Desc: Download webm from url either 249, 250 or 251
+Desc: Displays the available formats to download video
+Inputs: Video url
+Outputs: Video format
+
+*/
+
+string displayVideoFormats(string videoURL) {
+
+    stringstream command;
+    command << "youtube-dl -F " << videoURL;
+    system(command.str().c_str());
+
+    cout << "Format > ";
+    string videoFormat;
+    getline(cin, videoFormat);
+    return videoFormat;
+
+}
+
+
+/*
+
+Desc: Download webm from url
 Inputs: Video url and format number
 Outputs: None
 
@@ -51,6 +85,23 @@ void downloadVideo(string videoURL, string videoFormat) {
 
 /*
 
+Desc: Download mp4 from url
+Inputs: Video url, format number, and name of mp4
+Outputs: None
+
+*/
+
+void downloadVideo(string videoURL, string videoFormat, string mp4Name) {
+
+    stringstream command;
+    command << "youtube-dl -f " << videoFormat << " " << videoURL << " -o " << mp4Name;
+    system(command.str().c_str());
+
+}
+
+
+/*
+
 Desc: Converts webm to mp3 file and deletes webm
 Inputs: Name of mp3 file
 Outputs: none
@@ -63,6 +114,6 @@ void convertVideo(string mp3Name) {
     command << "ffmpeg -vn -i *.webm " << mp3Name << ".mp3";
     system(command.str().c_str());
 
-    system("rm *.webm");
+    system("rm -rf *.webm");
 
 }
