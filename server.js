b730051fc4781b0705953051bab0fda3f9a26375
Zachary Ness <magnuschase03@pop-os.localdomain>
2021-09-05 08:02:24 -0500

    Got a basic looking website started

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require("path")
 const app = express();
 const port = 3000;
 
+app.use(express.static(__dirname + "/assets"))
+
 app.get("/", (req, res) => {
 
     res.sendFile(path.join(__dirname, 'assets/index.html'));
